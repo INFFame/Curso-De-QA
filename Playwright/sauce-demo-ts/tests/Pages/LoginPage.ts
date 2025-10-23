e20a36a1b5f9181c66a501332cdf0bde531887f9
INFFame <128002442+INFFame@users.noreply.github.com>
2025-10-23 11:09:46 -0300

    Update comment

--- Playwright/sauce-demo-ts/tests/Pages/LoginPage.ts
+++ Playwright/sauce-demo-ts/tests/Pages/LoginPage.ts
@@ -3,18 +3,18 @@ import { type Locator, type Page } from '@playwright/test';
 export class LoginPage {
     // Elementos de la página de login
     readonly page: Page; // Referencia a la página
-    readonly username: Locator; // Localizador del campo de nombre de usuario
-    readonly password: Locator; // Localizador del campo de contraseña
+    readonly username: Locator; // Localizador nombre de usuario
+    readonly password: Locator; // Localizador contraseña
 
     constructor(page: Page) {
         this.page = page; // Inicializar la referencia a la página
-        this.username = page.locator('[data-test="username"]'); // Inicializar el localizador del campo de nombre de usuario
-        this.password = page.locator('[data-test="password"]'); // Inicializar el localizador del campo de contraseña
+        this.username = page.locator('[data-test="username"]'); // Inicializar el localizador nombre de usuario
+        this.password = page.locator('[data-test="password"]'); // Inicializar el localizador contraseña
     }
 
     async login(username: string, password: string) {
-        await this.username.fill(username);
-        await this.password.fill(password);
+        await this.username.fill(username); // Ingresar el nombre de usuario con metodo fill
+        await this.password.fill(password); // Ingresar la contraseña con metodo fill
     }
 
 }
